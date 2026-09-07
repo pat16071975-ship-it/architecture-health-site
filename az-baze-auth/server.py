4c6900420594586b694b22b308aec6b5f7dcdcb1
pat16071975-ship-it <pat16071975@gmail.com>
2026-09-07 13:15:12 +0700

    Исправить разрешение iframe аналитики услуг

--- az-baze-auth/server.py
+++ az-baze-auth/server.py
@@ -1,3 +1,5 @@
+from flask import request
+
 from app import app
 from report_storage import register_report_storage
 
@@ -6,6 +8,6 @@ register_report_storage(app)
 
 @app.after_request
 def allow_same_origin_report_frame(response):
-    if response.request.path == "/reports/services-base.html" if hasattr(response, "request") else False:
+    if request.path == "/reports/services-base.html":
         response.headers["X-Frame-Options"] = "SAMEORIGIN"
     return response
