7404f850f0e2e1c7bb665083fd1b1890d8bc72d3
pat16071975-ship-it <pat16071975@gmail.com>
2026-09-07 13:36:32 +0700

    Убрать лишние кнопки из аналитики услуг

--- az-baze-auth/server.py
+++ az-baze-auth/server.py
@@ -1,15 +1,28 @@
+import re
+
 from flask import request
 
 from app import app
-import report_storage
-from terminology import install as install_report_terminology
+from report_storage import register_report_storage
+from terminology import apply_report_terminology
 
-install_report_terminology(app, report_storage)
-report_storage.register_report_storage(app)
+register_report_storage(app)
+apply_report_terminology()
 
 
 @app.after_request
-def allow_same_origin_report_frame(response):
+def tune_report_response(response):
     if request.path == "/reports/services-base.html":
         response.headers["X-Frame-Options"] = "SAMEORIGIN"
+        if response.mimetype == "text/html":
+            html = response.get_data(as_text=True)
+            html = html.replace('<a class="btn" href="./">Управленческий отчёт</a>', '')
+            html = re.sub(
+                r'<a class="btn" target="_blank" rel="noopener" href="[^"]+">Прайс-классификатор</a>',
+                '',
+                html,
+            )
+            html = html.replace('<a class="btn" href="/reports/">К отчётам</a>', '')
+            html = html.replace('<a class="btn" href="/">На главную</a>', '')
+            response.set_data(html)
     return response
