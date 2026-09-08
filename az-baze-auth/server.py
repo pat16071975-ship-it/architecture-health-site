26205233867d81e6385c6af28bc885d3a442a04a
pat16071975-ship-it <pat16071975@gmail.com>
2026-09-08 15:49:31 +0700

    Подключить экономический контроль и favicon

--- az-baze-auth/server.py
+++ az-baze-auth/server.py
@@ -1,8 +1,9 @@
 import re
 
-from flask import request
+from flask import request, send_from_directory
 
-from app import app
+from app import SITE_ROOT, app
+import economics_control
 import finrez
 import ident_import
 import report_storage
@@ -12,6 +13,12 @@ report_storage.register_report_storage(app)
 terminology.install(app, report_storage)
 ident_import.register_ident_import(app)
 finrez.register_finrez(app)
+economics_control.register_economics_control(app)
+
+
+@app.get("/favicon.svg")
+def favicon_svg():
+    return send_from_directory(SITE_ROOT / "assets", "favicon.svg", mimetype="image/svg+xml")
 
 
 @app.after_request
@@ -44,5 +51,15 @@ def tune_report_response(response):
                 1,
             )
         response.set_data(html)
+
+    if response.mimetype == "text/html":
+        html = response.get_data(as_text=True)
+        if 'rel="icon"' not in html:
+            html = html.replace(
+                "</head>",
+                '<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=20260908-1">\n</head>',
+                1,
+            )
+            response.set_data(html)
 
     return response
