f12701d6ea345721fcc215b032ed6c84acdedd50
pat16071975-ship-it <pat16071975@gmail.com>
2026-09-09 15:40:58 +0700

    Управленческий отчет: подключить компактные карточки

--- az-baze-auth/server.py
+++ az-baze-auth/server.py
@@ -48,6 +48,12 @@ def tune_report_response(response):
             "mobile-date-fix.css?v=20260907-4",
             html,
         )
+        if "management-compact-metrics.css" not in html:
+            html = html.replace(
+                "</head>",
+                '<link rel="stylesheet" href="./management-compact-metrics.css?v=20260909-1">\n</head>',
+                1,
+            )
         if "management-mobile-layout.js" not in html:
             html = html.replace(
                 "</body>",
